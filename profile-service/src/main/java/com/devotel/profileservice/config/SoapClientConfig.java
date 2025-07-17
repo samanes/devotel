b27b285f66cb72a6a2f05cc0702a9007d8b36c5a
Saman Esmi <esmi.saman@gmail.com>
2025-07-17 09:44:34 +0400

    fix(profile-service): resolve SOAP connection by using USER_SERVICE_URI env var

--- profile-service/src/main/java/com/devotel/profileservice/config/SoapClientConfig.java
+++ profile-service/src/main/java/com/devotel/profileservice/config/SoapClientConfig.java
@@ -1,27 +1,35 @@
 package com.devotel.profileservice.config;
 
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
-import org.springframework.ws.client.core.WebServiceTemplate;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.ws.client.core.WebServiceTemplate;
 
 @Configuration
 public class SoapClientConfig {
 
+    private static final Logger log = LoggerFactory.getLogger(SoapClientConfig.class);
+
+    @Value("${soap.client.uri}")
+    private String userServiceUri;
+
     @Bean
-    public Jaxb2Marshaller marshaller() {
-        Jaxb2Marshaller m = new Jaxb2Marshaller();
-        m.setContextPath("com.devotel.userservice.soap.gen");
-        return m;
+    public Jaxb2Marshaller jaxb2Marshaller() {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        marshaller.setContextPath("com.devotel.userservice.soap.gen");
+        return marshaller;
     }
 
     @Bean
     public WebServiceTemplate webServiceTemplate(Jaxb2Marshaller marshaller) {
-        WebServiceTemplate wst = new WebServiceTemplate();
-        wst.setMarshaller(marshaller);
-        wst.setUnmarshaller(marshaller);
-        wst.setDefaultUri("http://localhost:8081/ws");
-        return wst;
+        WebServiceTemplate ws = new WebServiceTemplate();
+        ws.setMarshaller(marshaller);
+        ws.setUnmarshaller(marshaller);
+        ws.setDefaultUri(userServiceUri);
+        log.info(">>> WebServiceTemplate defaultUri set to {}", userServiceUri);
+        return ws;
     }
 }
